2eb9cc01e233ae192ca04de6218a14ebfc74b82e
Chaitanya <mnsvkchaitanya@gmail.com>
2017-07-07 10:37:23 +0530

    Cleaning

--- genomicOpenProject/ExeTest.R
+++
@@ -1,4 +0,0 @@
-a<-system2("AlphaBayes.exe" , args=c("--par", shQuote ("AlphaBayesSpec.txt") ), minimized = T, invisible =T, stdout="")
-a
-a<-system(paste("AlphaBayes.exe " , " --par AlphaBayesSpec.txt"))
-a
